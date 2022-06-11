865cf1f25226a4b613d5036023a0c6a198208d67
Samuel Hinga <samuelhinga200@gmail.com>
2022-06-12 02:40:00 +0300

    commit tree eb3c8f02ed03548a23b79036e6fcd7e1ee18d3f3

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,12 @@
-class Student 
+require "pry"
+require_relative "user"
+class Student < User
+  attr_reader :knowledge
 
+  def initialize
+    @knowledge = []
+  end
+  def learn(note)
+    @knowledge << note
+  end
 end
